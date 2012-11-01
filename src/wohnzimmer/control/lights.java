b3d3f3a7487c316e0d661b3e6169305977f8514f
André Hentschel <nerv@dawncrow.de>
2012-11-01 20:17:53 +0100

    read status from returned stream

--- src/wohnzimmer/control/lights.java
+++ src/wohnzimmer/control/lights.java
@@ -10,7 +10,9 @@ import android.widget.Toast;
 import java.net.URL;
 import java.net.HttpURLConnection;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
 
 public class lights extends Activity
@@ -53,6 +55,38 @@ public class lights extends Activity
                 rot2an = send_command(6, rot2an);
             }
         });
+
+        send_update();
+    }
+
+    private void send_update()
+    {
+        URL url;
+        HttpURLConnection urlConnection;
+        try {
+            url = new URL("http://192.168.178.32/main.cgi");
+        } catch (java.net.MalformedURLException e){
+            Toast toast = Toast.makeText(getApplicationContext(), "new URL failed: " + e.getMessage(), Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+        } catch (IOException e) {
+            Toast toast = Toast.makeText(getApplicationContext(), "openConnection failed: " + e.getMessage(), Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
+        try {
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            readStream(in);
+        } catch (IOException e) {
+            Toast toast = Toast.makeText(getApplicationContext(), "BufferedInputStream failed: " + e.getMessage(), Toast.LENGTH_SHORT);
+            toast.show();
+            urlConnection.disconnect();
+            return;
+        }
+        urlConnection.disconnect();
     }
 
     private int send_command(int bx, int an)
@@ -62,22 +96,22 @@ public class lights extends Activity
         try {
             url = new URL("http://192.168.178.32/main.cgi?bx"+bx+"="+an);
         } catch (java.net.MalformedURLException e){
-            Toast toast = Toast.makeText(getApplicationContext(), "new URL failed", Toast.LENGTH_SHORT);
+            Toast toast = Toast.makeText(getApplicationContext(), "new URL failed: " + e.getMessage(), Toast.LENGTH_SHORT);
             toast.show();
             return an;
         }
         try {
             urlConnection = (HttpURLConnection) url.openConnection();
         } catch (IOException e) {
-            Toast toast = Toast.makeText(getApplicationContext(), "openConnection failed", Toast.LENGTH_SHORT);
+            Toast toast = Toast.makeText(getApplicationContext(), "openConnection failed: " + e.getMessage(), Toast.LENGTH_SHORT);
             toast.show();
             return an;
         }
         try {
             InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-            /*readStream(in);*/
+            readStream(in);
         } catch (IOException e) {
-            Toast toast = Toast.makeText(getApplicationContext(), "BufferedInputStream failed", Toast.LENGTH_SHORT);
+            Toast toast = Toast.makeText(getApplicationContext(), "BufferedInputStream failed: " + e.getMessage(), Toast.LENGTH_SHORT);
             toast.show();
             urlConnection.disconnect();
             return an;
@@ -86,18 +120,84 @@ public class lights extends Activity
         if (an>0) return 0;
         else return 1;
     }
-/*
-private void readStream(InputStream is) {
-    try {
-      int i = is.read();
-      while(i != -1) {
-        i = is.read();
-      }
-      return;
-    } catch (IOException e) {
-Toast toast = Toast.makeText(getApplicationContext(), "wtf", Toast.LENGTH_SHORT);
-toast.show();
-      return;
+
+    private void readStream(InputStream is) {
+        try {
+            String line;
+            BufferedReader r = new BufferedReader(new InputStreamReader(is),250);  
+            while ((line = r.readLine()) != null) {
+                if(line.contains("script")==true && line.contains("ln")==true)
+                {
+                    char an = line.charAt(line.indexOf("bx") - 3);
+                    switch(line.charAt(line.indexOf("bx") + 2))
+                    {
+                        case '0':
+                            if (an == '1') blau2an=1; else blau2an=0;
+                            break;
+                        case '2':
+                            if (an == '1') blau1an=1; else blau1an=0;
+                            break;
+                        case '4':
+                            if (an == '1') rot1an=1; else rot1an=0;
+                            break;
+                        case '6':
+                            if (an == '1') rot2an=1; else rot2an=0;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            Toast toast = Toast.makeText(getApplicationContext(), "readStream failed: " + e.getMessage(), Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+        Button blue1;
+        blue1 = (Button) findViewById(R.id.blue1);
+        while (blue1 == null) blue1 = (Button) findViewById(R.id.blue1);
+        if (blau1an==1)
+        {
+            blue1.setTextColor(android.graphics.Color.BLACK);
+            blue1.setBackgroundColor(android.graphics.Color.BLUE);
+        }
+        else
+        {
+            blue1.setTextColor(android.graphics.Color.BLUE);
+            blue1.setBackgroundColor(android.graphics.Color.BLACK);
+        }
+        Button blue2 = (Button) findViewById(R.id.blue2);
+        if (blau2an==1)
+        {
+            blue2.setTextColor(android.graphics.Color.BLACK);
+            blue2.setBackgroundColor(android.graphics.Color.BLUE);
+        }
+        else
+        {
+            blue2.setTextColor(android.graphics.Color.BLUE);
+            blue2.setBackgroundColor(android.graphics.Color.BLACK);
+        }
+        Button red1 = (Button) findViewById(R.id.red1);
+        if (rot1an==1)
+        {
+            red1.setTextColor(android.graphics.Color.BLACK);
+            red1.setBackgroundColor(android.graphics.Color.RED);
+        }
+        else
+        {
+            red1.setTextColor(android.graphics.Color.RED);
+            red1.setBackgroundColor(android.graphics.Color.BLACK);
+        }
+        Button red2 = (Button) findViewById(R.id.red2);
+        if (rot2an==1)
+        {
+            red2.setTextColor(android.graphics.Color.BLACK);
+            red2.setBackgroundColor(android.graphics.Color.RED);
+        }
+        else
+        {
+            red2.setTextColor(android.graphics.Color.RED);
+            red2.setBackgroundColor(android.graphics.Color.BLACK);
+        }
     }
-}*/
 }
